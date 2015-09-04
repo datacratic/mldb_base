cd25656a496914f201baa6120aff2b435f0db012
Jean Raby <jean@datacratic.com>
2015-09-04 13:14:23 -0400

    rebuild_pycs: remove non-essential prints

--- rebuild_pycs.py
+++ rebuild_pycs.py
@@ -8,11 +8,9 @@ done_file = '/usr/local/lib/python2.7/dist-packages/.rebuild_pycs_done'
 if os.path.isfile(done_file):
   exit(0)
 
-print 'Rebuilding local .pyc files'
 for (dirpath, dirnames, files) in os.walk('/usr/local/lib/python2.7/dist-packages'):
     for file in files:
       if file.endswith('.py'):
         py_compile.compile(os.path.join(dirpath,file))
-print 'Done rebuilding local .pyc files'
 
 open(done_file, 'a')
