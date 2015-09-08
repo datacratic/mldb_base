61e5918345b53a3468096f08f1f9a970879a971c
Jean Raby <jean@datacratic.com>
2015-09-08 14:42:42 -0400

    Remove ppa:ubuntu-toolchain-r/test. Not used.

--- docker_create_mldb_base.sh
+++ docker_create_mldb_base.sh
@@ -58,7 +58,6 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y python-software-properties software-properties-common
 add-apt-repository -y ppa:nginx/stable
-#add-apt-repository -y ppa:ubuntu-toolchain-r/test
 apt-get update
 
 apt-get install -y \
